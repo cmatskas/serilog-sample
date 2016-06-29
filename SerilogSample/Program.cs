5d0de0d715990e242c8793cf9bdaf4e84cd605bb
Christos Matskas <chmatsk@microsoft.com>
2016-06-29 19:54:57 +0100

    initial commit

---
+++ SerilogSample/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Serilog;
+using static System.Console;
+
+namespace SerilogSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var storage = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            var log = new LoggerConfiguration()
+                .WriteTo.AzureTableStorage(storage)
+                .MinimumLevel.Debug()
+                .CreateLogger();
+
+            log.Debug("hello world");
+            log.Error("Oh no, an error");
+            log.Information("another message, just for info");
+
+            ReadKey();
+        }
+    }
+}
